c0a38e589e82ff031620143a470bf9560ded6da1
Jorge Aparicio <japaricious@gmail.com>
2016-09-02 20:46:17 -0500

    use new docker image, test windows and osx

--- ci/env.sh
+++ ci/env.sh
@@ -1,37 +1,28 @@
-export HOST=x86_64-unknown-linux-gnu
-export STAGING_DIR=/openwrt/staging_dir
-
-case $TARGET in
-    aarch64-unknown-linux-gnu)
-        export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
+case $TRAVIS_OS_NAME in
+    linux)
+        HOST=x86_64-unknown-linux-gnu
+        LINUX=1
         ;;
-    arm-unknown-linux-gnueabi)
-        export QEMU_LD_PREFIX=/usr/arm-linux-gnueabi
-        ;;
-    arm*-unknown-linux-gnueabihf)
-        export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
-        ;;
-    mips-unknown-linux-gnu)
-        export QEMU_LD_PREFIX=/usr/mips-linux-gnu
-        ;;
-    mipsel-unknown-linux-gnu)
-        export QEMU_LD_PREFIX=/usr/mipsel-linux-gnu
-        ;;
-    mips*-unknown-linux-musl)
-        export CC_${TARGET//-/_}=${TARGET//unknown-/}-gcc
-        CONFIGURE_CARGO=y
-        if [[ -d $STAGING_DIR ]]; then
-            export QEMU_LD_PREFIX=$(echo $STAGING_DIR/toolchain-*/)
-        fi
-        ;;
-    powerpc-unknown-linux-gnu)
-        export QEMU_LD_PREFIX=/usr/powerpc-linux-gnu
-        ;;
-    powerpc64-unknown-linux-gnu)
-        export CC_${TARGET//-/_}=${TARGET//unknown-/}-gcc
-        export QEMU_LD_PREFIX=/usr/powerpc64-linux-gnu
-        ;;
-    powerpc64le-unknown-linux-gnu)
-        export QEMU_LD_PREFIX=/usr/powerpc64le-linux-gnu
+    osx)
+        HOST=x86_64-apple-darwin
+        OSX=1
         ;;
 esac
+
+if [[ $LINUX ]]; then
+    case $TARGET in
+        aarch64-unknown-linux-gnu |\
+            arm-unknown-linux-gnueabi |\
+            arm*-unknown-linux-gnueabihf |\
+            mips*-unknown-linux-gnu |\
+            powerpc*-unknown-linux-gnu)
+            QEMU_LD_PREFIX=${TARGET//unknown-/}
+            ;;
+        mips*-unknown-linux-musl)
+            export STAGING_DIR=/openwrt/$TARGET/staging_dir
+            QEMU_LD_PREFIX=$(echo $STAGING_DIR/toolchain-*)
+            ;;
+    esac
+
+    export QEMU_LD_PREFIX
+fi
