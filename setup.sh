78345fbb274232eae49f0d816ecf9aae22299923
Jorge Aparicio <japaricious@gmail.com>
2016-09-11 15:22:16 -0500

    install gcc-multilib first
    apparently this fixes some dependency problems

--- setup.sh
+++ setup.sh
@@ -3,12 +3,15 @@ set -ex
 main() {
     apt-get update
 
+    # i*86-unknown-linux-gnu
+    apt-get install --no-install-recommends -y gcc-multilib
+
     apt-get install --no-install-recommends -y \
             `# QEMU` binfmt-support qemu-user-static \
             `# aarch64-unknown-linux-gnu` gcc-aarch64-linux-gnu libc6-dev-arm64-cross \
             `# arm*-unknown-linux-gnueabihf` gcc-arm-linux-gnueabihf libc6-dev-armhf-cross \
             `# arm-unknown-linux-gnueabi` gcc-arm-linux-gnueabi libc6-dev-armel-cross \
-            `# i*86-unknown-linux-gnu` gcc-multilib libc6-dev-i386 \
+            `# i*86-unknown-linux-gnu` libc6-dev-i386 \
             `# i686-unknown-linux-musl` make \
             `# mips*-unknown-linux-musl` bzip2 \
             `# mips-unknown-linux-gnu` gcc-mips-linux-gnu libc6-dev-mips-cross \
