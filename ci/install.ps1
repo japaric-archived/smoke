895ed0259361c00c5dca5026c2e2de6bc14682b7
Jorge Aparicio <japaricious@gmail.com>
2016-10-17 01:17:40 -0500

    use trust's CI template as base for our CI

--- ci/install.ps1
+++ ci/install.ps1
@@ -1,32 +1,28 @@
-Set-PSDebug -Trace 1
+If ($Env:APPVEYOR_REPO_TAG -eq 'false' -Or $Env:RUST_VERSION -eq $Env:DEPLOY_VERSION) {
+    $Env:HOST = $Env:TARGET
 
-if ($Env:TARGET -Match 'gnu') {
-    $Env:HOST = 'x86_64-pc-windows-gnu'
-
-    if ($Env:TARGET -Match 'x86_64') {
-        $Env:PATH += ';C:\msys64\mingw64\bin'
-    } else {
-        $Env:PATH += ';C:\msys64\mingw32\bin'
+    If ($Env:TARGET -Match 'gnu') {
+        if ($Env:TARGET -Match 'x86_64') {
+            $Env:PATH += ';C:\msys64\mingw64\bin'
+        } else {
+            $Env:PATH += ';C:\msys64\mingw32\bin'
+        }
+    } ElseIf ($Env:TARGET -Match 'i586') {
+        $Env:HOST = 'i686-pc-windows-msvc'
     }
+
+    [Net.ServicePointManager]::SecurityProtocol = 'Ssl3, Tls, Tls12'
+    Start-FileDownload 'https://win.rustup.rs' 'rustup-init.exe'
+
+    .\rustup-init --default-host $Env:HOST --default-toolchain $Env:RUST_VERSION -y
+
+    $Env:PATH = 'C:\Users\appveyor\.cargo\bin;' + $Env:PATH
+
+    If ($Env:TARGET -ne $Env:HOST) {
+        rustup target add $Env:TARGET
+    }
+
+    rustc -Vv
+
+    cargo -V
 }
-
-if ($Env:TARGET -Match 'msvc') {
-    $Env:HOST = 'x86_64-pc-windows-msvc'
-}
-
-[Net.ServicePointManager]::SecurityProtocol = 'Ssl3, Tls, Tls12'
-Start-FileDownload 'https://win.rustup.rs' 'rustup-init.exe'
-
-.\rustup-init --default-host $Env:HOST --default-toolchain nightly -y
-
-$Env:PATH = 'C:\Users\appveyor\.cargo\bin;' + $Env:PATH
-
-if ($Env:TARGET -ne $Env:HOST) {
-    rustup target add $Env:TARGET
-}
-
-$Env:PATH
-
-rustc -Vv
-
-cargo -V
